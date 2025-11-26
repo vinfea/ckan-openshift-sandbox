203e01615e7defe336dac82b207fbf5461c8b266
Christa Huang <christa.huang@agr.gc.ca>
2025-11-26 20:02:16 +0000

    fix url for csa extension

--- ckan-base/docker-entrypoint.d/05_setup_gov_theme.sh
+++ ckan-base/docker-entrypoint.d/05_setup_gov_theme.sh
@@ -10,7 +10,7 @@ echo "--------------------------------------------------------------------------
 ckan config-tool "$CKAN_INI" \
     "scheming.dataset_schemas=ckanext.scheming:ckan_dataset.json ckanext.csa:info.json ckanext.csa:doc.json" \
     "scheming.presets=ckanext.scheming:presets.json ckanext.fluent:presets.json" \
-    "licenses_group_url=file:///usr/lib/ckan/default/src/ckanext-asc-csa/ckanext/csa/public/licenses.json" \
+    "licenses_group_url=file:///srv/app/src/ckanext-asc-csa/ckanext/csa/public/licenses.json" \
     "ckan.locale_default=en" \
     "ckan.locale_order=en fr" \
     "ckan.locales_offered=en fr"
